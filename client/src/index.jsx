8480ebed2f85bde9e8180aacfc0cd72d9cf7e4db
chrisamurao <freshpotatoes@gmail.com>
2018-07-28 07:34:06 -0700

    saving commit

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,48 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import ZipForm from './components/ZipForm.jsx';
-import LoginForm from './components/LoginForm.jsx';
-import ListView from './components/ListView.jsx';
-import MapContainer from './components/MapContainer.jsx';
-import TownHallContainer from './components/townhall/TownHallContainer.jsx';
 import './App.css';
 import Routes from './components/routes/index.jsx';
-
-const styles = {
-  master: {
-    alignContent: "center",
-    fontFamily: "Verdana, Geneva, sans-serif"
-  },
-  headers: {
-    backgroundColor: "red",
-    alignContent: "center",
-    color: "white",
-    margin: 0,
-    padding: 10
-  },
-  zip: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: 40,
-    paddingTop: 100
-  }
-};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: '',
-      tier: 'state',
-      currentView: ''
+      isLoggedIn: false,
     }
   }
 
   render () {
-    return <div style={styles.master}>
-        <div className="nav" style={styles.headers}>
+    return <div className="master">
+        <div className="headers">
           <h1>App v1.1</h1>
         </div>
         <Routes />
