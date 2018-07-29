a450b4639605e2daaa0a415bae5f341b449d1da1
chrisamurao <freshpotatoes@gmail.com>
2018-07-29 08:26:47 -0700

    basic enzyme setup

---
+++ client/src/__tests__/TestComponent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { shallow, mount } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+export default class TestComponent extends Component {
+  render() {
+    return (
+      <div>
+        Hello Friends!
+      </div>
+    )
+  }
+}
+describe('does TestComponent work', () => {
+  it('should contain a div with hello world in it', () => {
+    const wrapper = mount(<TestComponent />);
+    expect(wrapper.text()).toEqual('Hello Friends!');
+  });
+})
---
+++ client/src/__tests__/simple.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import TestComponent from './TestComponent.jsx';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+// describe('<MyComponent />', () => {
+//   it('renders three <Foo /> components', () => {
+//     const wrapper = shallow(<MyComponent />);
+//     expect(wrapper.find(Foo)).to.have.length(3);
+//   });
+
+//   it('renders an `.icon-star`', () => {
+//     const wrapper = shallow(<MyComponent />);
+//     expect(wrapper.find('.icon-star')).to.have.length(1);
+//   });
+
+//   it('renders children when passed in', () => {
+//     const wrapper = shallow((
+//       <MyComponent>
+//         <div className="unique" />
+//       </MyComponent>
+//     ));
+//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+//   });
+
+//   it('simulates click events', () => {
+//     const onButtonClick = sinon.spy();
+//     const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+//     wrapper.find('button').simulate('click');
+//     expect(onButtonClick).to.have.property('callCount', 1);
+//   });
+// });
