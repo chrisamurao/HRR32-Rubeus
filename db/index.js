ce87b6fa4b8fa37ae7ce5bc1fef1dfffc954b342
chrisamurao <freshpotatoes@gmail.com>
2018-07-30 07:31:05 -0700

    heroku stuff

--- db/index.js
+++ db/index.js
@@ -4,9 +4,6 @@ const SQL_USER = process.env.SQL_USER || 'root';
 const SQL_PASS = process.env.SQL_PASS || '';
 const CLEARDB_DATABASE_URL = process.env.CLEARDB_DATABASE_URL || 'localhost';
 
-
-mysql://b42c64e282667c:d6ea66b1@us-cdbr-iron-east-04.cleardb.net/heroku_d8331a8cbcce2bc?reconnect=true
-
 const connection = mysql.createConnection(CLEARDB_DATABASE_URL);
 
 /*
